ce9db9d212ef80f3cf384ddb8110427c96910cfe
chandlerpatterson <cpatte@rams.colostate.edu>
2020-11-16 22:49:58 -0700

    13.1 11/17 initial commit, boostrap, and header

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <div class="container-fluid">
+      <app-header></app-header>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components:{
+    "app-header": Header
+    }
+}
+</script>
+
+<style>
+/* #app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+} */
+</style>
